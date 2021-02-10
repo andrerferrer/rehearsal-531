a04898d359ed519663be3643786ac4afa2f8df09
andre ferrer <andrerferrer@hotmail.com>
2021-02-10 07:29:13 -0300

    finish livecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello from src/index.js!");
+// 1st select the element
+const btn = document.querySelector('#click-me');
+// console.log(btn);
+
+// 2nd add event listener
+// btn.addEventListener('event', 'behavior')
+btn.addEventListener('click', (event) => {
+  // console.log(event); // click ITSELF
+  // console.log(event.currentTarget); // element that we clicked on
+  // btn == event.currentTarget
+  btn.innerText = 'Wait please...';
+  event.currentTarget.disabled = true;
+});
